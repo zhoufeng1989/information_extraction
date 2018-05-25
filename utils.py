b277a875b9267784faaa448e5e65b2be736131cd
zhoufeng1989 <zhoufengloop@gmail.com>
2018-05-25 20:54:04 +1200

    Refactor code.

--- utils.py
+++ utils.py
@@ -1,10 +1,11 @@
 from nltk import sent_tokenize, word_tokenize
 
 
-def tokenize(filename):
-    text = open(filename).read()
+def tokenize(text):
     sents = sent_tokenize(text)
     tokenized_sents = [word_tokenize(sent) for sent in sents]
     return tokenized_sents
 
 
+class NERNotFound(Exception):
+    pass
